7dcbeab78737ab253bc8c6a02584d315b8c9bb83
thamindukiridana <thamindukiridana@gmail.com>
2022-05-17 00:33:47 +0530

    Implement persistent storage

---
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/impl/PersistentAccountDAO.java
@@ -0,0 +1,132 @@
+package lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.AccountDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.exception.InvalidAccountException;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Account;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.ExpenseType;
+
+
+/**
+ * PersistentAccountDAO can be used to persistently access the account details, including listing, adding, updating, removing
+ * accounts and updating account balance.
+ */
+public class PersistentAccountDAO implements AccountDAO {
+    private SQLiteDatabase db;
+
+    public PersistentAccountDAO(SQLiteDatabase db) {
+        this.db = db;
+    }
+
+    /***
+     * Get a list of account numbers.
+     */
+    @Override
+    public List<String> getAccountNumbersList() {
+
+        Cursor curAcc = db.rawQuery("SELECT account_number FROM account", null);
+
+        List<String> accountNumbers = new ArrayList<String>();
+
+        if (curAcc.moveToFirst()) {
+            do {
+                accountNumbers.add(curAcc.getString(curAcc.getColumnIndex("account_number")));
+            } while (curAcc.moveToNext());
+        }
+
+        return accountNumbers;
+    }
+
+    /***
+     * Get a list of accounts.
+     */
+    @Override
+    public List<Account> getAccountsList() {
+        Cursor curAcc = db.rawQuery("SELECT * FROM account", null);
+
+        List<Account> accounts = new ArrayList<Account>();
+
+        if (curAcc.moveToFirst()) {
+            do {
+                Account acc = new Account(curAcc.getString(curAcc.getColumnIndex("account_number")),
+                        curAcc.getString(curAcc.getColumnIndex("bank")),
+                        curAcc.getString(curAcc.getColumnIndex("holder")),
+                        curAcc.getDouble(curAcc.getColumnIndex("balance")));
+                accounts.add(acc);
+            } while (curAcc.moveToNext());
+        }
+        return accounts;
+    }
+
+    /***
+     * Get the account given the account number.
+     */
+    @Override
+    public Account getAccount(String accountNo) throws InvalidAccountException {
+        Cursor curAcc = db.rawQuery("SELECT * FROM account WHERE account_number = " + accountNo, null);
+        Account acc = null;
+
+        if (curAcc.moveToFirst()) {
+            do {
+                acc = new Account(curAcc.getString(curAcc.getColumnIndex("account_number")),
+                        curAcc.getString(curAcc.getColumnIndex("bank")),
+                        curAcc.getString(curAcc.getColumnIndex("holder")),
+                        curAcc.getDouble(curAcc.getColumnIndex("balance")));
+            } while (curAcc.moveToNext());
+        }
+    return acc;
+    }
+
+    /***
+     * Add an account to the accounts collection.
+     */
+    @Override
+    public void addAccount(Account account) {
+        String prepStmt = "INSERT INTO account (account_number, bank, holder, balance) VALUES (?, ?, ?, ?)";
+        SQLiteStatement sqlStmt = db.compileStatement(prepStmt);
+
+        sqlStmt.bindString(1, account.getAccountNo());
+        sqlStmt.bindString(2, account.getBankName());
+        sqlStmt.bindString(3, account.getAccountHolderName());
+        sqlStmt.bindDouble(4, account.getBalance());
+
+        sqlStmt.executeInsert();
+    }
+
+    /***
+     * Remove an account from the accounts collection.
+     */
+    @Override
+    public void removeAccount(String accountNo) throws InvalidAccountException {
+        String prepStmt = "DELETE FROM account WHERE account_number = ?";
+        SQLiteStatement sqlStmt = db.compileStatement(prepStmt);
+
+        sqlStmt.bindString(1, accountNo);
+
+        sqlStmt.executeUpdateDelete();
+    }
+
+    /***
+     * Update the balance of the given account.
+     */
+    @Override
+    public void updateBalance(String accountNo, ExpenseType expenseType, double amount) throws InvalidAccountException {
+        String prepStmt = "UPDATE account SET balance = balance + ?";
+        SQLiteStatement sqlStmt = db.compileStatement(prepStmt);
+
+        if (expenseType == ExpenseType.EXPENSE) {
+            sqlStmt.bindDouble(1, -amount);
+        } else {
+            sqlStmt.bindDouble(1, amount);
+        }
+
+        sqlStmt.executeUpdateDelete();
+    }
+
+}
---
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/impl/PersistentTransactionDAO.java
@@ -0,0 +1,69 @@
+package lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.TransactionDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.ExpenseType;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Transaction;
+
+public class PersistentTransactionDAO implements TransactionDAO {
+    private SQLiteDatabase db;
+
+    public PersistentTransactionDAO(SQLiteDatabase db) {
+        this.db = db;
+    }
+
+    @Override
+    public void logTransaction(Date date, String accountNo, ExpenseType expenseType, double amount) {
+        String prepStmt = "INSERT INTO transactionRecord (account_number, date, type, amount) VALUES (?, ?, ?, ?)";
+        SQLiteStatement sqlStmt = db.compileStatement(prepStmt);
+
+        sqlStmt.bindString(1, accountNo);
+        sqlStmt.bindLong(2, date.getTime());
+        sqlStmt.bindLong(3, (expenseType == ExpenseType.EXPENSE) ? 0 : 1);
+        sqlStmt.bindDouble(4, amount);
+
+        sqlStmt.executeInsert();
+    }
+
+    @Override
+    public List<Transaction> getAllTransactionLogs() {
+        Cursor curTrs = db.rawQuery("SELECT * FROM transactionRecord", null);
+        List<Transaction> transactions = new ArrayList<Transaction>();
+
+        if (curTrs.moveToFirst()) {
+            do {
+                Transaction trs = new Transaction(new Date(curTrs.getLong(curTrs.getColumnIndex("date"))),
+                        curTrs.getString(curTrs.getColumnIndex("account_number")),
+                        (curTrs.getInt(curTrs.getColumnIndex("type")) == 0) ? ExpenseType.EXPENSE : ExpenseType.INCOME,
+                        curTrs.getDouble(curTrs.getColumnIndex("amount")));
+                transactions.add(trs);
+            } while (curTrs.moveToNext());
+        }
+        return transactions;
+    }
+
+    @Override
+    public List<Transaction> getPaginatedTransactionLogs(int limit) {
+        Cursor curTrs = db.rawQuery("SELECT * FROM transactionRecord LIMIT " + limit, null);
+        List<Transaction> transactions = new ArrayList<Transaction>();
+
+        if (curTrs.moveToFirst()) {
+            do {
+                Transaction trs = new Transaction(new Date(curTrs.getLong(curTrs.getColumnIndex("date"))),
+                        curTrs.getString(curTrs.getColumnIndex("account_number")),
+                        (curTrs.getInt(curTrs.getColumnIndex("type")) == 0) ? ExpenseType.EXPENSE : ExpenseType.INCOME,
+                        curTrs.getDouble(curTrs.getColumnIndex("amount")));
+                transactions.add(trs);
+            } while (curTrs.moveToNext());
+        }
+        return transactions;
+    }
+
+}
